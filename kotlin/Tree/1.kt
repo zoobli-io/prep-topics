259ee9251cd718f7d949990202e1e1d573d05618
Dimske <info@zoobli.io>
2022-12-22 22:08:45 +0100

    Create 1.kt
    Signed-off-by: Dimske <info@zoobli.io>

---
+++ kotlin/Tree/1.kt
@@ -0,0 +1,18 @@
+class TreeNode<T>(value:T){
+    var value:T = value
+    var parent:TreeNode<T>? = null
+    var children:MutableList<TreeNode<T>> = mutableListOf()
+
+    fun addChild(node:TreeNode<T>){
+        children.add(node)
+        node.parent = this
+    }
+
+    override fun toString(): String {
+        var s = "${value}"
+        if (!children.isEmpty()) {
+            s += " {" + children.map { it.toString() } + " }"
+        }
+        return s
+    }
+}
